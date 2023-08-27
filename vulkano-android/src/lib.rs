8481c88fde8ccb2e600df98aa972f6c0ab965ea7
SSSxCCC <570909477@qq.com>
2023-08-27 20:11:37 +0800

    Prepare to combine android and windows client
    Signed-off-by: SSSxCCC <570909477@qq.com>

--- vulkano-android/src/lib.rs
+++ vulkano-android/src/lib.rs
@@ -3,29 +3,26 @@ use winit::{
     event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
 };
 #[cfg(target_os = "android")]
-use {winit::platform::android::activity::AndroidApp,
-    log::info};
+use winit::platform::android::activity::AndroidApp;
 
-#[allow(dead_code)]
 #[cfg(target_os = "android")]
 #[no_mangle]
 fn android_main(app: AndroidApp) {
+    android_logger::init_once(android_logger::Config::default().with_max_level(log::LevelFilter::Trace));
     use winit::platform::android::EventLoopBuilderExtAndroid;
-
-    android_logger::init_once(android_logger::Config::default().with_min_level(log::Level::Info));
-
     let event_loop = EventLoopBuilder::new().with_android_app(app).build().unwrap();
     _main(event_loop);
 }
 
-#[allow(dead_code)]
 #[cfg(not(target_os = "android"))]
 fn main() {
+    env_logger::builder().filter_level(log::LevelFilter::Trace).parse_default_env().init();
     let event_loop = EventLoopBuilder::new().build().unwrap();
     _main(event_loop);
 }
 
 fn _main(event_loop: EventLoop<()>) {
+    log::warn!("Vulkano start main loop!");
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent { event: WindowEvent::CloseRequested, .. } => {
             *control_flow = ControlFlow::Exit;
@@ -33,6 +30,6 @@ fn _main(event_loop: EventLoop<()>) {
         Event::WindowEvent { event: WindowEvent::Resized(_), .. } => {
             //vulkan_app.notify_window_resized();
         }
-        _ => (),
-    });
+        _ => log::warn!("AAABBBCCC"),
+    }).unwrap();
 }
