b6bf0f3515a2abd1c2bba1d77aef8d93f43256a1
SSSxCCC <570909477@qq.com>
2023-11-10 18:34:29 +0800

    Add specs (unfinished)
    Signed-off-by: SSSxCCC <570909477@qq.com>

--- steel/src/steel.rs
+++ steel/src/steel.rs
@@ -1,38 +1,42 @@
 use std::sync::Arc;
-
+use specs::prelude::*;
 use rapier2d::prelude::*;
 
 pub struct Engine {
-
+    scene: Option<Arc<Scene>>,
+    managers: Vec<Box<dyn Manager>>,
 }
 
 impl Engine {
-    pub fn new() -> Engine {
-        Engine{}
+    pub fn new() -> Arc<Self> {
+        let managers: Vec<Box<dyn Manager>> = vec![Box::new(Physics2DManager::new())];
+        Arc::new(Engine { scene: None, managers })
     }
 
-    pub fn init(&self) {
+    pub fn init(self: &Arc<Self>) {
         println!("Engine::init");
 
-        let mut physics2d_system = Physics2DSystem::new();
+        self.scene = Some(Scene::new(self.clone()));
+
+        let mut physics2d_manager = Physics2DManager::new();
 
         /* Create the ground. */
         let collider = ColliderBuilder::cuboid(100.0, 0.1).build();
-        physics2d_system.collider_set.insert(collider);
+        physics2d_manager.collider_set.insert(collider);
 
         /* Create the bouncing ball. */
         let rigid_body = RigidBodyBuilder::dynamic()
                 .translation(vector![0.0, 10.0])
                 .build();
         let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
-        let ball_body_handle = physics2d_system.rigid_body_set.insert(rigid_body);
-        physics2d_system.collider_set.insert_with_parent(collider, ball_body_handle, &mut physics2d_system.rigid_body_set);
+        let ball_body_handle = physics2d_manager.rigid_body_set.insert(rigid_body);
+        physics2d_manager.collider_set.insert_with_parent(collider, ball_body_handle, &mut physics2d_manager.rigid_body_set);
 
         /* Run the game loop, stepping the simulation once per frame. */
         for _ in 0..200 {
-            physics2d_system.update();
+            physics2d_manager.on_update();
 
-            let ball_body = &physics2d_system.rigid_body_set[ball_body_handle];
+            let ball_body = &physics2d_manager.rigid_body_set[ball_body_handle];
             println!("Ball altitude: {}", ball_body.translation().y);
         }
     }
@@ -46,12 +50,25 @@ impl Engine {
     }
 }
 
+trait Manager {
+    fn new() -> Self where Self: Sized;
+    fn on_update(&mut self) { }
+}
+
 struct Scene {
+    engine: Arc<Engine>,
     objects: Vec<Arc<Object>>,
+}
+
+impl Scene {
+    fn new(engine: Arc<Engine>) -> Arc<Self> {
+        Arc::new(Scene { engine, objects: Vec::new() })
+    }
 }
 
 struct Object {
     behaviours: Vec<Arc<dyn Behaviour>>,
+    scene: Arc<Scene>,
 }
 
 impl Object {
@@ -61,6 +78,7 @@ impl Object {
 }
 
 trait Behaviour {
+    fn new() -> Arc<dyn Behaviour> where Self: Sized;
     fn on_create(&mut self, _object: Arc<Object>) { }
     fn on_update(&mut self) { }
     fn on_draw(&mut self) { }
@@ -68,14 +86,21 @@ trait Behaviour {
 }
 
 struct RigidBody2D {
-
+    object: Option<Arc<Object>>,
 }
 
 impl Behaviour for RigidBody2D {
-    
+    fn new() -> Arc<dyn Behaviour> {
+        Arc::new(RigidBody2D { object: None })
+    }
+
+    fn on_create(&mut self, object: Arc<Object>) {
+        self.object = Some(object);
+        object.scene.engine.get
+    }
 }
 
-struct Physics2DSystem {
+struct Physics2DManager {
     rigid_body_set: RigidBodySet,
     collider_set: ColliderSet,
     gravity: Vector<Real>,
@@ -91,16 +116,16 @@ struct Physics2DSystem {
     event_handler: Box<dyn EventHandler>,
 }
 
-impl Physics2DSystem {
-    fn new() -> Physics2DSystem {
-        Physics2DSystem { rigid_body_set: RigidBodySet::new(), collider_set: ColliderSet::new(), gravity: vector![0.0, -9.81],
+impl Manager for Physics2DManager {
+    fn new() -> Self {
+        Physics2DManager { rigid_body_set: RigidBodySet::new(), collider_set: ColliderSet::new(), gravity: vector![0.0, -9.81],
             integration_parameters: IntegrationParameters::default(), physics_pipeline: PhysicsPipeline::new(),
             island_manager: IslandManager::new(), broad_phase: BroadPhase::new(), narrow_phase: NarrowPhase::new(),
             impulse_joint_set: ImpulseJointSet::new(), multibody_joint_set: MultibodyJointSet::new(),
             ccd_solver: CCDSolver::new(), physics_hooks: Box::new(()), event_handler: Box::new(()) }
     }
 
-    fn update(&mut self) {
+    fn on_update(&mut self) {
         self.physics_pipeline.step(
             &self.gravity,
             &self.integration_parameters,
