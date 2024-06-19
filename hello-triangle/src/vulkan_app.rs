2440bb6ed6cf3e63806d1b6fb46590a16c210580
SSSxCCC <570909477@qq.com>
2024-06-19 17:37:54 +0800

    Update vulkano version of hello-triangle to 3.4.
    Signed-off-by: SSSxCCC <570909477@qq.com>

--- hello-triangle/src/vulkan_app.rs
+++ hello-triangle/src/vulkan_app.rs
@@ -13,33 +13,37 @@
 
 use std::sync::Arc;
 
-use vulkano::VulkanLibrary;
+use vulkano::pipeline::graphics::color_blend::{ColorBlendAttachmentState, ColorBlendState};
+use vulkano::pipeline::graphics::multisample::MultisampleState;
+use vulkano::pipeline::graphics::rasterization::RasterizationState;
+use vulkano::pipeline::graphics::GraphicsPipelineCreateInfo;
+use vulkano::pipeline::layout::PipelineDescriptorSetLayoutCreateInfo;
+use vulkano::{Validated, VulkanError, VulkanLibrary};
 use vulkano::buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage, Subbuffer};
 use vulkano::command_buffer::allocator::StandardCommandBufferAllocator;
 use vulkano::command_buffer::{
-    AutoCommandBufferBuilder, CommandBufferExecFuture, CommandBufferUsage,
-    PrimaryAutoCommandBuffer, RenderPassBeginInfo, SubpassContents,
+    AutoCommandBufferBuilder, CommandBufferExecFuture, CommandBufferUsage, PrimaryAutoCommandBuffer, RenderPassBeginInfo, SubpassBeginInfo, SubpassContents
 };
 use vulkano::device::physical::{PhysicalDevice, PhysicalDeviceType};
 use vulkano::device::{
     Device, DeviceCreateInfo, DeviceExtensions, Queue, QueueCreateInfo, QueueFlags,
 };
 use vulkano::image::view::ImageView;
-use vulkano::image::{ImageUsage, SwapchainImage};
+use vulkano::image::{Image, ImageUsage};
 use vulkano::instance::{Instance, InstanceCreateInfo};
-use vulkano::memory::allocator::{AllocationCreateInfo, MemoryUsage, StandardMemoryAllocator};
+use vulkano::memory::allocator::{AllocationCreateInfo, MemoryTypeFilter, StandardMemoryAllocator};
 use vulkano::pipeline::graphics::input_assembly::InputAssemblyState;
-use vulkano::pipeline::graphics::vertex_input::Vertex;
-use vulkano::pipeline::graphics::viewport::{Viewport, ViewportState};
-use vulkano::pipeline::GraphicsPipeline;
+use vulkano::pipeline::graphics::vertex_input::{Vertex, VertexDefinition};
+use vulkano::pipeline::graphics::viewport::{Scissor, Viewport, ViewportState};
+use vulkano::pipeline::{GraphicsPipeline, PipelineLayout, PipelineShaderStageCreateInfo};
 use vulkano::render_pass::{Framebuffer, FramebufferCreateInfo, RenderPass, Subpass};
 use vulkano::shader::ShaderModule;
 use vulkano::swapchain::{
-    self, AcquireError, PresentFuture, Surface, Swapchain, SwapchainAcquireFuture,
-    SwapchainCreateInfo, SwapchainCreationError, SwapchainPresentInfo,
+    self, PresentFuture, Surface, Swapchain, SwapchainAcquireFuture,
+    SwapchainCreateInfo, SwapchainPresentInfo,
 };
 use vulkano::sync::future::{FenceSignalFuture, JoinFuture};
-use vulkano::sync::{self, FlushError, GpuFuture};
+use vulkano::sync::{self, GpuFuture};
 use winit::window::Window;
 
 #[derive(BufferContents, Vertex)]
@@ -89,10 +93,10 @@ pub struct VulkanApp {
     device: Arc<Device>,
     queue: Arc<Queue>,
     swapchain: Arc<Swapchain>,
-    swapchain_images: Vec<Arc<SwapchainImage>>,
+    swapchain_images: Vec<Arc<Image>>,
     render_pass: Arc<RenderPass>,
     framebuffers: Vec<Arc<Framebuffer>>,
-    memory_allocator: StandardMemoryAllocator,
+    memory_allocator: Arc<StandardMemoryAllocator>,
 
     vertex_buffer: Subbuffer<[MyVertex]>,
     vs: Arc<ShaderModule>,
@@ -112,17 +116,16 @@ pub struct VulkanApp {
 impl VulkanApp {
     pub fn new(window: Arc<Window>) -> VulkanApp {
         let library = vulkano::VulkanLibrary::new().expect("no local Vulkan library/DLL");
-        let required_extensions = vulkano_win::required_extensions(&library);
+        let required_extensions = Surface::required_extensions(window.as_ref());
         let instance = Instance::new(
             library.clone(),
             InstanceCreateInfo {
                 enabled_extensions: required_extensions,
                 ..Default::default()
             },
-        )
-        .expect("failed to create instance");
+        ).expect("failed to create instance");
 
-        let surface = vulkano_win::create_surface_from_winit(window.clone(), instance.clone()).expect("failed to create surface");
+        let surface = Surface::from_window(instance.clone(), window.clone()).expect("failed to create surface");
 
         let device_extensions = DeviceExtensions {
             khr_swapchain: true,
@@ -154,12 +157,10 @@ impl VulkanApp {
     
             let dimensions = window.inner_size();
             let composite_alpha = caps.supported_composite_alpha.into_iter().next().unwrap();
-            let image_format = Some(
-                physical_device
-                    .surface_formats(&surface, Default::default())
-                    .unwrap()[0]
-                    .0,
-            );
+            let image_format = physical_device
+                .surface_formats(&surface, Default::default())
+                .unwrap()[0]
+                .0;
     
             Swapchain::new(
                 device.clone(),
@@ -179,7 +180,7 @@ impl VulkanApp {
         let render_pass = Self::get_render_pass(device.clone(), swapchain.clone());
         let framebuffers = Self::get_framebuffers(&swapchain_images, render_pass.clone());
     
-        let memory_allocator = StandardMemoryAllocator::new_default(device.clone());
+        let memory_allocator = Arc::new(StandardMemoryAllocator::new_default(device.clone()));
     
         let vertex1 = MyVertex {
             position: [-0.5, -0.5],
@@ -191,28 +192,28 @@ impl VulkanApp {
             position: [0.5, -0.25],
         };
         let vertex_buffer = Buffer::from_iter(
-            &memory_allocator,
+            memory_allocator.clone(),
             BufferCreateInfo {
                 usage: BufferUsage::VERTEX_BUFFER,
                 ..Default::default()
             },
             AllocationCreateInfo {
-                usage: MemoryUsage::Upload,
+                memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                    | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
                 ..Default::default()
             },
             vec![vertex1, vertex2, vertex3].into_iter(),
-        )
-        .unwrap();
+        ).unwrap();
     
         let vs = vs::load(device.clone()).expect("failed to create shader module");
         let fs = fs::load(device.clone()).expect("failed to create shader module");
-    
+
         let viewport = Viewport {
-            origin: [0.0, 0.0],
-            dimensions: window.inner_size().into(),
-            depth_range: 0.0..1.0,
+            offset: [0.0, 0.0],
+            extent: window.inner_size().into(),
+            depth_range: 0.0..=1.0,
         };
-    
+
         let pipeline = Self::get_pipeline(
             device.clone(),
             vs.clone(),
@@ -220,10 +221,10 @@ impl VulkanApp {
             render_pass.clone(),
             viewport.clone(),
         );
-    
+
         let command_buffer_allocator =
             StandardCommandBufferAllocator::new(device.clone(), Default::default());
-    
+
         let command_buffers = Self::get_command_buffers(
             &command_buffer_allocator,
             &queue,
@@ -272,22 +273,21 @@ impl VulkanApp {
             device,
             attachments: {
                 color: {
-                    load: Clear,
-                    store: Store,
                     format: swapchain.image_format(), // set the format the same as the swapchain
                     samples: 1,
+                    load_op: Clear,
+                    store_op: Store,
                 },
             },
             pass: {
                 color: [color],
                 depth_stencil: {},
             },
-        )
-        .unwrap()
+        ).unwrap()
     }
     
     fn get_framebuffers(
-        images: &[Arc<SwapchainImage>],
+        images: &[Arc<Image>],
         render_pass: Arc<RenderPass>,
     ) -> Vec<Arc<Framebuffer>> {
         images
@@ -305,7 +305,7 @@ impl VulkanApp {
             })
             .collect::<Vec<_>>()
     }
-    
+
     fn get_pipeline(
         device: Arc<Device>,
         vs: Arc<ShaderModule>,
@@ -313,17 +313,39 @@ impl VulkanApp {
         render_pass: Arc<RenderPass>,
         viewport: Viewport,
     ) -> Arc<GraphicsPipeline> {
-        GraphicsPipeline::start()
-            .vertex_input_state(MyVertex::per_vertex())
-            .vertex_shader(vs.entry_point("main").unwrap(), ())
-            .input_assembly_state(InputAssemblyState::new())
-            .viewport_state(ViewportState::viewport_fixed_scissor_irrelevant([viewport]))
-            .fragment_shader(fs.entry_point("main").unwrap(), ())
-            .render_pass(Subpass::from(render_pass, 0).unwrap())
-            .build(device)
-            .unwrap()
+        let vs = vs.entry_point("main").unwrap();
+        let fs = fs.entry_point("main").unwrap();
+        let vertex_input_state = MyVertex::per_vertex().definition(&vs.info().input_interface).unwrap();
+        let stages = [
+            PipelineShaderStageCreateInfo::new(vs),
+            PipelineShaderStageCreateInfo::new(fs),
+        ];
+        let layout = PipelineLayout::new(
+            device.clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages(&stages)
+                .into_pipeline_layout_create_info(device.clone())
+                .unwrap(),
+        ).unwrap();
+        let subpass = Subpass::from(render_pass, 0).unwrap();
+        GraphicsPipeline::new(device.clone(), None, GraphicsPipelineCreateInfo {
+            stages: stages.into_iter().collect(),
+            vertex_input_state: Some(vertex_input_state),
+            input_assembly_state: Some(InputAssemblyState::default()),
+            rasterization_state: Some(RasterizationState::default()),
+            multisample_state: Some(MultisampleState::default()),
+            color_blend_state: Some(ColorBlendState::with_attachment_states(
+                subpass.num_color_attachments(),
+                ColorBlendAttachmentState::default())),
+            viewport_state: Some(ViewportState {
+                viewports: [viewport].into_iter().collect(),
+                scissors: [Scissor::default()].into_iter().collect(),
+                ..Default::default()
+            }),
+            subpass: Some(subpass.into()),
+            ..GraphicsPipelineCreateInfo::layout(layout)
+        }).unwrap()
     }
-    
+
     fn get_command_buffers(
         command_buffer_allocator: &StandardCommandBufferAllocator,
         queue: &Arc<Queue>,
@@ -338,8 +360,7 @@ impl VulkanApp {
                     command_buffer_allocator,
                     queue.queue_family_index(),
                     CommandBufferUsage::MultipleSubmit,
-                )
-                .unwrap();
+                ).unwrap();
     
                 builder
                     .begin_render_pass(
@@ -347,19 +368,18 @@ impl VulkanApp {
                             clear_values: vec![Some([0.0, 0.0, 1.0, 1.0].into())],
                             ..RenderPassBeginInfo::framebuffer(framebuffer.clone())
                         },
-                        SubpassContents::Inline,
-                    )
-                    .unwrap()
-                    .bind_pipeline_graphics(pipeline.clone())
-                    .bind_vertex_buffers(0, vertex_buffer.clone())
-                    .draw(vertex_buffer.len() as u32, 1, 0, 0)
-                    .unwrap()
-                    .end_render_pass()
-                    .unwrap();
-    
-                Arc::new(builder.build().unwrap())
-            })
-            .collect()
+                        SubpassBeginInfo {
+                            contents: SubpassContents::Inline,
+                            ..Default::default()
+                        },
+                    ).unwrap()
+                    .bind_pipeline_graphics(pipeline.clone()).unwrap()
+                    .bind_vertex_buffers(0, vertex_buffer.clone()).unwrap()
+                    .draw(vertex_buffer.len() as u32, 1, 0, 0).unwrap()
+                    .end_render_pass(Default::default()).unwrap();
+
+                builder.build().unwrap()
+            }).collect()
     }
 
     pub fn notify_window_resized(&mut self) {
@@ -377,7 +397,6 @@ impl VulkanApp {
                 ..self.swapchain.create_info()
             }) {
                 Ok(r) => r,
-                Err(SwapchainCreationError::ImageExtentNotSupported { .. }) => return,
                 Err(e) => panic!("failed to recreate swapchain: {e}"),
             };
             self.swapchain = new_swapchain;
@@ -386,7 +405,7 @@ impl VulkanApp {
             if self.window_resized {
                 self.window_resized = false;
 
-                self.viewport.dimensions = new_dimensions.into();
+                self.viewport.extent = new_dimensions.into();
                 let new_pipeline = Self::get_pipeline(
                     self.device.clone(),
                     self.vs.clone(),
@@ -407,7 +426,7 @@ impl VulkanApp {
         let (image_i, suboptimal, acquire_future) =
             match swapchain::acquire_next_image(self.swapchain.clone(), None) {
                 Ok(r) => r,
-                Err(AcquireError::OutOfDate) => {
+                Err(Validated::Error(VulkanError::OutOfDate)) => {
                     self.recreate_swapchain = true;
                     return;
                 }
@@ -447,7 +466,7 @@ impl VulkanApp {
 
         self.fences[image_i as usize] = match future {
             Ok(value) => Some(Arc::new(value)),
-            Err(FlushError::OutOfDate) => {
+            Err(Validated::Error(VulkanError::OutOfDate)) => {
                 self.recreate_swapchain = true;
                 None
             }
