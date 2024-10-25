51b56409e8e1982a4680a7eca673642c592e6c72
SSSxCCC <570909477@qq.com>
2024-10-25 15:35:31 +0800

    Create "texture" project.
    Signed-off-by: SSSxCCC <570909477@qq.com>

---
+++ texture/src/lib.rs
@@ -0,0 +1,87 @@
+use vulkano_util::{
+    context::VulkanoContext,
+    window::{VulkanoWindows, WindowDescriptor},
+};
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop, EventLoopBuilder},
+};
+mod triangle_renderer;
+use triangle_renderer::TriangleRenderer;
+
+#[cfg(target_os = "android")]
+use winit::platform::android::activity::AndroidApp;
+
+#[cfg(target_os = "android")]
+#[no_mangle]
+fn android_main(app: AndroidApp) {
+    android_logger::init_once(
+        android_logger::Config::default().with_max_level(log::LevelFilter::Trace),
+    );
+    use winit::platform::android::EventLoopBuilderExtAndroid;
+    let event_loop = EventLoopBuilder::new().with_android_app(app).build();
+    _main(event_loop);
+}
+
+#[cfg(not(target_os = "android"))]
+#[allow(dead_code)]
+fn main() {
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Trace)
+        .parse_default_env()
+        .init();
+    let event_loop = EventLoopBuilder::new().build();
+    _main(event_loop);
+}
+
+fn _main(event_loop: EventLoop<()>) {
+    let context = VulkanoContext::default();
+    let mut windows = VulkanoWindows::default();
+
+    log::warn!("Vulkano start main loop!");
+    event_loop.run(
+        move |event: Event<'_, ()>, event_loop, control_flow| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                ..
+            } => {
+                log::debug!("WindowEvent::CloseRequested");
+                *control_flow = ControlFlow::Exit;
+            }
+            Event::Resumed => {
+                log::debug!("Resumed");
+                windows.create_window(&event_loop, &context, &WindowDescriptor::default(), |_| {});
+            }
+            Event::Suspended => {
+                log::debug!("Suspended");
+                windows.remove_renderer(windows.primary_window_id().unwrap());
+            }
+            Event::WindowEvent {
+                event: WindowEvent::Resized(_),
+                ..
+            } => {
+                log::debug!("WindowEvent::Resized");
+                if let Some(renderer) = windows.get_primary_renderer_mut() {
+                    renderer.resize()
+                }
+            }
+            Event::RedrawRequested(_) => {
+                log::trace!("RedrawRequested");
+                if let Some(renderer) = windows.get_primary_renderer_mut() {
+                    let before_future = renderer.acquire().unwrap();
+
+                    let after_future = TriangleRenderer::draw(before_future, &context, renderer);
+
+                    renderer.present(after_future, true);
+                }
+            }
+            Event::MainEventsCleared => {
+                log::trace!("MainEventsCleared");
+                if let Some(renderer) = windows.get_primary_renderer() {
+                    renderer.window().request_redraw()
+                }
+            }
+            _ => (),
+        },
+    );
+}
---
+++ texture/src/triangle_renderer.rs
@@ -0,0 +1,218 @@
+use vulkano::{
+    buffer::{Buffer, BufferContents, BufferCreateInfo, BufferUsage},
+    command_buffer::{
+        allocator::StandardCommandBufferAllocator, AutoCommandBufferBuilder, CommandBufferUsage,
+        PrimaryCommandBufferAbstract, RenderPassBeginInfo, SubpassBeginInfo, SubpassContents,
+    },
+    memory::allocator::{AllocationCreateInfo, MemoryTypeFilter},
+    pipeline::{
+        graphics::{
+            color_blend::{ColorBlendAttachmentState, ColorBlendState},
+            input_assembly::InputAssemblyState,
+            multisample::MultisampleState,
+            rasterization::RasterizationState,
+            vertex_input::{Vertex, VertexDefinition},
+            viewport::{Viewport, ViewportState},
+            GraphicsPipelineCreateInfo,
+        },
+        layout::PipelineDescriptorSetLayoutCreateInfo,
+        DynamicState, GraphicsPipeline, PipelineLayout, PipelineShaderStageCreateInfo,
+    },
+    render_pass::{Framebuffer, FramebufferCreateInfo, Subpass},
+    sync::GpuFuture,
+};
+use vulkano_util::{context::VulkanoContext, renderer::VulkanoWindowRenderer};
+
+#[derive(BufferContents, Vertex)]
+#[repr(C)]
+struct MyVertex {
+    #[format(R32G32_SFLOAT)]
+    position: [f32; 2],
+}
+
+mod vs {
+    vulkano_shaders::shader! {
+        ty: "vertex",
+        src: r"
+            #version 460
+
+            layout(location = 0) in vec2 position;
+
+            void main() {
+                gl_Position = vec4(position, 0.0, 1.0);
+            }
+        ",
+    }
+}
+
+mod fs {
+    vulkano_shaders::shader! {
+        ty: "fragment",
+        src: r"
+            #version 460
+
+            layout(location = 0) out vec4 f_color;
+
+            void main() {
+                f_color = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        ",
+    }
+}
+
+pub struct TriangleRenderer;
+
+impl TriangleRenderer {
+    pub fn draw(
+        before_future: Box<dyn GpuFuture>,
+        context: &VulkanoContext,
+        renderer: &VulkanoWindowRenderer,
+    ) -> Box<dyn GpuFuture> {
+        let render_pass = vulkano::single_pass_renderpass!(
+            context.device().clone(),
+            attachments: {
+                color: {
+                    format: renderer.swapchain_format(), // set the format the same as the swapchain
+                    samples: 1,
+                    load_op: Clear,
+                    store_op: Store,
+                },
+            },
+            pass: {
+                color: [color],
+                depth_stencil: {},
+            },
+        )
+        .unwrap();
+
+        let framebuffer = Framebuffer::new(
+            render_pass.clone(),
+            FramebufferCreateInfo {
+                attachments: vec![renderer.swapchain_image_view()],
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+        let vertex1 = MyVertex {
+            position: [-0.5, -0.5],
+        };
+        let vertex2 = MyVertex {
+            position: [0.0, 0.5],
+        };
+        let vertex3 = MyVertex {
+            position: [0.5, -0.25],
+        };
+        let vertex_buffer = Buffer::from_iter(
+            context.memory_allocator().clone(),
+            BufferCreateInfo {
+                usage: BufferUsage::VERTEX_BUFFER,
+                ..Default::default()
+            },
+            AllocationCreateInfo {
+                memory_type_filter: MemoryTypeFilter::PREFER_DEVICE
+                    | MemoryTypeFilter::HOST_SEQUENTIAL_WRITE,
+                ..Default::default()
+            },
+            vec![vertex1, vertex2, vertex3].into_iter(),
+        )
+        .unwrap();
+
+        let vs = vs::load(context.device().clone())
+            .unwrap()
+            .entry_point("main")
+            .unwrap();
+        let fs = fs::load(context.device().clone())
+            .unwrap()
+            .entry_point("main")
+            .unwrap();
+
+        let vertex_input_state = MyVertex::per_vertex()
+            .definition(&vs.info().input_interface)
+            .unwrap();
+
+        let stages = [
+            PipelineShaderStageCreateInfo::new(vs),
+            PipelineShaderStageCreateInfo::new(fs),
+        ];
+
+        let layout = PipelineLayout::new(
+            context.device().clone(),
+            PipelineDescriptorSetLayoutCreateInfo::from_stages(&stages)
+                .into_pipeline_layout_create_info(context.device().clone())
+                .unwrap(),
+        )
+        .unwrap();
+
+        let viewport = Viewport {
+            offset: [0.0, 0.0],
+            extent: renderer.window().inner_size().into(),
+            depth_range: 0.0..=1.0,
+        };
+
+        let subpass = Subpass::from(render_pass.clone(), 0).unwrap();
+
+        let pipeline = GraphicsPipeline::new(
+            context.device().clone(),
+            None,
+            GraphicsPipelineCreateInfo {
+                stages: stages.into_iter().collect(),
+                vertex_input_state: Some(vertex_input_state),
+                input_assembly_state: Some(InputAssemblyState::default()),
+                rasterization_state: Some(RasterizationState::default()),
+                multisample_state: Some(MultisampleState::default()),
+                color_blend_state: Some(ColorBlendState::with_attachment_states(
+                    subpass.num_color_attachments(),
+                    ColorBlendAttachmentState::default(),
+                )),
+                viewport_state: Some(ViewportState::default()),
+                dynamic_state: [DynamicState::Viewport].into_iter().collect(),
+                subpass: Some(subpass.into()),
+                ..GraphicsPipelineCreateInfo::layout(layout)
+            },
+        )
+        .unwrap();
+
+        let command_buffer_allocator =
+            StandardCommandBufferAllocator::new(context.device().clone(), Default::default());
+
+        let command_buffer = {
+            let mut builder = AutoCommandBufferBuilder::primary(
+                &command_buffer_allocator,
+                renderer.graphics_queue().queue_family_index(),
+                CommandBufferUsage::OneTimeSubmit,
+            )
+            .unwrap();
+
+            builder
+                .begin_render_pass(
+                    RenderPassBeginInfo {
+                        clear_values: vec![Some([0.0, 0.0, 1.0, 1.0].into())],
+                        ..RenderPassBeginInfo::framebuffer(framebuffer.clone())
+                    },
+                    SubpassBeginInfo {
+                        contents: SubpassContents::Inline,
+                        ..Default::default()
+                    },
+                )
+                .unwrap()
+                .set_viewport(0, [viewport].into_iter().collect())
+                .unwrap()
+                .bind_pipeline_graphics(pipeline.clone())
+                .unwrap()
+                .bind_vertex_buffers(0, vertex_buffer.clone())
+                .unwrap()
+                .draw(vertex_buffer.len() as u32, 1, 0, 0)
+                .unwrap()
+                .end_render_pass(Default::default())
+                .unwrap();
+
+            builder.build().unwrap()
+        };
+
+        return command_buffer
+            .execute_after(before_future, renderer.graphics_queue())
+            .unwrap()
+            .boxed();
+    }
+}
